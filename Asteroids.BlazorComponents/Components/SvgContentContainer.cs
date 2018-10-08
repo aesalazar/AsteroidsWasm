d31b3b79d23183695b614110466f36e1cd780a58
Ernie Salazar <salazare@verizon.net>
2018-10-08 18:25:01 -0400

    SvgContentContainer cleanup

--- Asteroids.BlazorComponents/Components/SvgContentContainer.cs
+++ Asteroids.BlazorComponents/Components/SvgContentContainer.cs
@@ -8,11 +8,23 @@ using Microsoft.AspNetCore.Blazor.RenderTree;
 namespace Asteroids.BlazorComponents.Components
 {
     /// <summary>
-    /// Provides raw graphical contain for an HTML SVG element.
+    /// Provides raw graphical content for an HTML SVG element.
     /// </summary>
     public class SvgContentContainer : BlazorComponent
     {
         private const string StylePolygon = "stroke=width:1px; fill:transparent; ";
+
+        private const string AttributeLine = "line";
+        private const string AttributeLineX1 = "x1";
+        private const string AttributeLineY1 = "y1";
+        private const string AttributeLineX2 = "x2";
+        private const string AttributeLineY2 = "y2";
+
+        private const string AttributePolygon = "polygon";
+        private const string AttributePoints = "points";
+
+        private const string AttributeStyle = "style";
+        private const string AttributeStroke = "stroke";
 
         private IEnumerable<IGraphicLine> _lastLines = new List<IGraphicLine>();
         private IEnumerable<IGraphicPolygon> _lastPolygons = new List<IGraphicPolygon>();
@@ -34,8 +46,10 @@ namespace Asteroids.BlazorComponents.Components
         /// <param name="polygons">Collection of <see cref="IGraphicPolygon"/>.</param>
         public void Draw(IEnumerable<IGraphicLine> lines, IEnumerable<IGraphicPolygon> polygons)
         {
+            //Full replace to avoid locks
             _lastLines = lines;
             _lastPolygons = polygons;
+
             StateHasChanged();
         }
 
@@ -45,20 +59,26 @@ namespace Asteroids.BlazorComponents.Components
         /// <param name="builder">A <see cref="RenderTreeBuilder"/> that will receive the render output.</param>
         protected override void BuildRenderTree(RenderTreeBuilder builder)
         {
+            //Grab the refs in case multi-threading becomes an option
+            var lines = _lastLines;
+            var polygons = _lastPolygons;
+
             var count = 0;
 
-            foreach (var line in _lastLines)
+            //Add the lines
+            foreach (var line in lines)
             {
-                builder.OpenElement(count++, nameof(line));
-                builder.AddAttribute(count++, "x1", line.Point1.X);
-                builder.AddAttribute(count++, "y1", line.Point1.Y);
-                builder.AddAttribute(count++, "x2", line.Point2.X);
-                builder.AddAttribute(count++, "y2", line.Point2.Y);
-                builder.AddAttribute(count++, "style", $"stroke:{line.ColorHex}");
+                builder.OpenElement(count++, AttributeLine);
+                builder.AddAttribute(count++, AttributeLineX1, line.Point1.X);
+                builder.AddAttribute(count++, AttributeLineY1, line.Point1.Y);
+                builder.AddAttribute(count++, AttributeLineX2, line.Point2.X);
+                builder.AddAttribute(count++, AttributeLineY2, line.Point2.Y);
+                builder.AddAttribute(count++, AttributeStyle, $"{AttributeStroke}:{line.ColorHex}");
                 builder.CloseElement();
             }
 
-            foreach (var polygon in _lastPolygons)
+            //add the polygons
+            foreach (var polygon in polygons)
             {
                 var points = polygon
                     .Points
@@ -67,12 +87,11 @@ namespace Asteroids.BlazorComponents.Components
                         , (sb, p) => sb.Append($"{p.X},{p.Y} ")
                     );
 
-                builder.OpenElement(count++, nameof(polygon));
-                builder.AddAttribute(count++, nameof(points), points.ToString());
-                builder.AddAttribute(count++, "style", $"stroke:{polygon.ColorHex}; {StylePolygon}");
+                builder.OpenElement(count++, AttributePolygon);
+                builder.AddAttribute(count++, AttributePoints, points.ToString());
+                builder.AddAttribute(count++, AttributeStyle, $"{AttributeStroke}:{polygon.ColorHex}; {StylePolygon}");
                 builder.CloseElement();
             }
-
 
             base.BuildRenderTree(builder);
 
