e09b164ed52088c8d4bcac0e087100216fb735b5
Ernie Salazar <salazare@verizon.net>
2018-10-09 20:36:19 -0400

    docs folder with published verison of wasm

---
+++ docs/_content/Asteroids.BlazorComponents/JsInteropAsteroidsKeyPress.js
@@ -0,0 +1,9 @@
+﻿document.onkeydown = function (evt) {
+    evt = evt || window.event;
+    DotNet.invokeMethodAsync('Asteroids.BlazorComponents', 'JsKeyDown', evt.keyCode);
+};
+
+document.onkeyup = function (evt) {
+    evt = evt || window.event;
+    DotNet.invokeMethodAsync('Asteroids.BlazorComponents', 'JsKeyUp', evt.keyCode);
+};
