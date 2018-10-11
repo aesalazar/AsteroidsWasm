9b4bafd36de42ca2bcf825b08b5d79e82fd353f8
Ernie Salazar <salazare@verizon.net>
2018-10-11 18:21:08 -0400

    removed defunct interop key press

--- Asteroids.BlazorComponents/content/JsInteropAsteroidsKeyPress.js
+++
@@ -1,9 +0,0 @@
-﻿document.onkeydown = function (evt) {
-    evt = evt || window.event;
-    DotNet.invokeMethodAsync('Asteroids.BlazorComponents', 'JsKeyDown', evt.keyCode);
-};
-
-document.onkeyup = function (evt) {
-    evt = evt || window.event;
-    DotNet.invokeMethodAsync('Asteroids.BlazorComponents', 'JsKeyUp', evt.keyCode);
-};
