4223f82089f9df6996d83b3d666ac83408ca2a79
Ernie Salazar <salazare@verizon.net>
2018-10-11 20:00:33 -0400

    Revert "removed defunct interop key press"
    This reverts commit 9b4bafd36de42ca2bcf825b08b5d79e82fd353f8.

---
+++ Asteroids.BlazorComponents/JsInterop/InteropKeyPress.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.JSInterop;
+
+namespace Asteroids.BlazorComponents.JsInterop
+{
+    /// <summary>
+    /// Proxy for receiving key press events from JavaScript.
+    /// </summary>
+    public static class InteropKeyPress
+    {
+        /// <summary>
+        /// Fires when a KeyUp message is received from JavaScript.
+        /// </summary>
+        public static event EventHandler<ConsoleKey> KeyUp;
+
+        /// <summary>
+        /// Fires when a KeyDown message is received from JavaScript.
+        /// </summary>
+        public static event EventHandler<ConsoleKey> KeyDown;
+
+        /// <summary>
+        /// Called by JavaScript when a Key Down event fires.
+        /// </summary>
+        /// <param name="e"><see cref="ConsoleKey"/> number.</param>
+        /// <returns>
+        /// JavaScript Promise with the converted <see cref="ConsoleKey"/> value or <see cref="null"/> if
+        /// no equivalent is found.
+        /// </returns>
+        [JSInvokable]
+        public static Task<bool> JsKeyDown(int e)
+        {
+            var found = false;
+            var consoleKey = default(ConsoleKey);
+
+            try
+            {
+                consoleKey = (ConsoleKey)e;
+                found = true;
+            }
+            catch
+            {
+                Console.WriteLine($"Cound not find {nameof(ConsoleKey)} for JS key value {e})");
+            }
+
+            if (found)
+                KeyDown?.Invoke(null, consoleKey);
+
+            return Task.FromResult(found);
+        }
+
+        /// <summary>
+        /// Called by JavaScript when a Key Up event fires.
+        /// </summary>
+        /// <param name="e"><see cref="ConsoleKey"/> number.</param>
+        /// <returns>
+        /// JavaScript Promise with the converted <see cref="ConsoleKey"/> value or <see cref="null"/> if
+        /// no equivalent is found.
+        /// </returns>
+        [JSInvokable]
+        public static Task<bool> JsKeyUp(int e)
+        {
+            var found = false;
+            var consoleKey = default(ConsoleKey);
+
+            try
+            {
+                consoleKey = (ConsoleKey)e;
+                found = true;
+            }
+            catch
+            {
+                Console.WriteLine($"Cound not find {nameof(ConsoleKey)} for JS key value {e})");
+            }
+
+            if (found)
+                KeyUp?.Invoke(null, consoleKey);
+
+            return Task.FromResult(found);
+        }
+    }
+}
