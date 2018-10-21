282fbe20df62a045a02918324131523c6116c3f8
Ernie Salazar <salazare@verizon.net>
2018-10-21 15:19:45 -0400

    updated docs folder

--- docs/_content/Asteroids.BlazorComponents/JsInteropAsteroidsKeyPress.js
+++ docs/_content/Asteroids.BlazorComponents/JsInteropAsteroidsKeyPress.js
@@ -1,9 +1,17 @@
 ﻿document.onkeydown = function (evt) {
     evt = evt || window.event;
     DotNet.invokeMethodAsync('Asteroids.BlazorComponents', 'JsKeyDown', evt.keyCode);
+
+    //Prevent all but F5 and F12
+    if (evt.keyCode !== 116 && evt.keyCode !== 123)
+        evt.preventDefault();
 };
 
 document.onkeyup = function (evt) {
     evt = evt || window.event;
     DotNet.invokeMethodAsync('Asteroids.BlazorComponents', 'JsKeyUp', evt.keyCode);
+
+    //Prevent all but F5 and F12
+    if (evt.keyCode !== 116 && evt.keyCode !== 123)
+        evt.preventDefault();
 };
