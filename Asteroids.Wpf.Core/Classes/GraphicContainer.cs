d574e875faabfb04a5698368cdb5d4b2a3dcc823
Ernie Salazar <salazare@verizon.net>
2019-04-27 07:38:02 -0400

    rebuilt wpf.core with core 3 preview 4

---
+++ Asteroids.Wpf.Core/Classes/GraphicContainer.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Threading;
+using Asteroids.Standard.Interfaces;
+
+namespace Asteroids.Wpf.Core.Classes
+{
+    /// <summary>
+    /// Control to paint vectors based on <see cref="WriteableBitmap"/>.
+    /// </summary>
+    public class GraphicContainer : Image, IGraphicContainer, IDisposable
+    {
+        private readonly Dispatcher _mainDispatcher = Dispatcher.CurrentDispatcher;
+        private WriteableBitmap _bitmap;
+
+        /// <summary>
+        /// Creates a new instance of <see cref="GraphicContainer"/>.
+        /// </summary>
+        public GraphicContainer()
+        {
+            SizeChanged += OnSizeChanged;
+        }
+
+        /// <summary>
+        /// Reisize the <see cref="WriteableBitmap"/> based on new control size.
+        /// </summary>
+        private void OnSizeChanged(object sender, System.Windows.SizeChangedEventArgs e)
+        {
+            //Resize the current bitmap
+            _bitmap = _bitmap.Resize(
+                (int)e.NewSize.Width
+                , (int)e.NewSize.Height
+                , WriteableBitmapExtensions.Interpolation.Bilinear
+            );
+
+            Source = _bitmap;
+        }
+
+        /// <summary>
+        /// Initialize the <see cref="WriteableBitmap"/> with the current width and height.
+        /// </summary>
+        public Task Initialize()
+        {
+            //Since the control has no size yet simply draw a size bitmap
+            _bitmap = BitmapFactory.New(0, 0);
+            Source = _bitmap;
+            return Task.CompletedTask;
+        }
+
+        /// <summary>
+        /// Draws the collection of <see cref="IGraphicLine"/>s and <see cref="IGraphicPolygon"/>s
+        /// to the screen.
+        /// </summary>
+        public async Task Draw(IEnumerable<IGraphicLine> lines, IEnumerable<IGraphicPolygon> polygons)
+        {
+            try
+            {
+                await _mainDispatcher.InvokeAsync(() =>
+                {
+                    _bitmap.Clear();
+
+                    foreach (var gline in lines)
+                    {
+                        _bitmap.DrawLine(
+                            gline.Point1.X
+                            , gline.Point1.Y
+                            , gline.Point2.X
+                            , gline.Point2.Y
+                            , HexToColor(gline.ColorHex)
+                        );
+                    }
+
+                    foreach (var gpoly in polygons)
+                    {
+                        var points = new int[gpoly.Points.Count * 2 + 2];
+
+                        for (int i = 0, c = 0; i < points.Length - 2; i += 2, c++)
+                        {
+                            var p = gpoly.Points[c];
+                            points[i] = p.X;
+                            points[i + 1] = p.Y;
+                        }
+
+                        var first = gpoly.Points.First();
+                        points[points.Length - 2] = first.X;
+                        points[points.Length - 1] = first.Y;
+
+                        _bitmap.DrawPolyline(
+                            points
+                            , HexToColor(gpoly.ColorHex)
+                        );
+                    }
+                });
+            }
+            catch (Exception)
+            {
+                //Ignore
+            }
+        }
+
+        /// <summary>
+        /// Cleanup handlers.
+        /// </summary>
+        public void Dispose()
+        {
+            SizeChanged -= OnSizeChanged;
+        }
+
+        #region Color
+
+        private string _lastColorHex;
+        private Color _lastColor;
+
+        /// <summary>
+        /// Converts color hex string to <see cref="Color"/>.
+        /// </summary>
+        private Color HexToColor(string colorHex)
+        {
+            if (colorHex == _lastColorHex)
+                return _lastColor;
+
+            _lastColorHex = colorHex;
+            _lastColor = (Color)ColorConverter.ConvertFromString(_lastColorHex);
+
+            return _lastColor;
+        }
+
+        #endregion
+    }
+}
---
+++ Asteroids.Wpf.Core/MainWindow.xaml.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Media;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+using Asteroids.Standard;
+using Asteroids.Standard.Enums;
+using Asteroids.Standard.Interfaces;
+
+namespace Asteroids.Wpf.Core
+{
+    public partial class MainWindow : IDisposable
+    {
+        private readonly IGameController _controller;
+        private readonly IDictionary<ActionSound, SoundPlayer> _soundPlayers;
+        private SoundPlayer _soundPlaying;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            _controller = new GameController();
+
+            _soundPlayers = Standard
+                .Sounds.ActionSounds.SoundDictionary
+                .ToDictionary(
+                    kvp => kvp.Key
+                    , kvp => new SoundPlayer(kvp.Value)
+                );
+
+            foreach (var player in _soundPlayers)
+                player.Value.Load();
+
+            _controller.SoundPlayed += OnSoundPlayed;
+        }
+
+        private void OnSoundPlayed(object sender, ActionSound sound)
+        {
+            if (_soundPlaying != null)
+                return;
+
+            _soundPlaying = _soundPlayers[sound];
+
+            Task.Factory.StartNew(() =>
+            {
+                _soundPlaying.Stream.Position = 0;
+                _soundPlaying.PlaySync();
+                _soundPlaying = null;
+            });
+        }
+
+        private async void Window_Rendered(object sender, EventArgs e)
+        {
+            ContentRendered -= Window_Rendered;
+            var rec = new Rectangle(0, 0, (int)MainContainer.ActualWidth, (int)MainContainer.ActualHeight);
+            await _controller.Initialize(MainContainer, rec);
+        }
+
+        private void Window_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            _controller.ResizeGame(new Rectangle(0, 0, (int)e.NewSize.Width, (int)e.NewSize.Height));
+        }
+
+        private void Window_KeyDown(object sender, KeyEventArgs e)
+        {
+            PlayKey key;
+            switch (e.Key)
+            {
+                case Key.Escape:
+                    // Escape during a title screen exits the game
+                    if (_controller.GameStatus == GameMode.Title)
+                    {
+                        Application.Current.Shutdown();
+                        return;
+                    }
+
+                    key = PlayKey.Escape;
+                    break;
+
+                case Key.Left:
+                    key = PlayKey.Left;
+                    break;
+
+                case Key.Right:
+                    key = PlayKey.Right;
+                    break;
+
+                case Key.Up:
+                    key = PlayKey.Up;
+                    break;
+
+                case Key.Down:
+                    key = PlayKey.Down;
+                    break;
+
+                case Key.Space:
+                    key = PlayKey.Space;
+                    break;
+
+                case Key.P:
+                    key = PlayKey.P;
+                    break;
+
+                default:
+                    return;
+            }
+
+            _controller.KeyDown(key);
+        }
+
+        private void Window_KeyUp(object sender, KeyEventArgs e)
+        {
+            PlayKey key;
+            switch (e.Key)
+            {
+                case Key.Escape:
+                    key = PlayKey.Escape;
+                    break;
+
+                case Key.Left:
+                    key = PlayKey.Left;
+                    break;
+
+                case Key.Right:
+                    key = PlayKey.Right;
+                    break;
+
+                case Key.Up:
+                    key = PlayKey.Up;
+                    break;
+
+                case Key.Down:
+                    key = PlayKey.Down;
+                    break;
+
+                case Key.Space:
+                    key = PlayKey.Space;
+                    break;
+
+                case Key.P:
+                    key = PlayKey.P;
+                    break;
+
+                default:
+                    return;
+            }
+
+            _controller.KeyUp(key);
+        }
+
+        public void Dispose()
+        {
+            _controller.SoundPlayed -= OnSoundPlayed;
+
+            foreach (var player in _soundPlayers)
+                player.Value.Dispose();
+
+            _controller.Dispose();
+            MainContainer.Dispose();
+        }
+    }
+}
