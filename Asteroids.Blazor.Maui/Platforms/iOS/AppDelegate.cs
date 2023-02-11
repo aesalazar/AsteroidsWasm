c1328cf9d803e93aea77cd6514cbefc6511d73bc
aesalazar <salazare@verizon.net>
2023-02-11 11:33:07 -0500

    blazor maui implemention
    This is using the project generation wizard and post-processing.  The
    demo web components were removed and a reference to the
    Asteroids.BlazorComponents project added.  All of the weather
    demo app content was removed and replaced with the asteroids
    GraphicsContainer, similar to how the other blazor apps work.

---
+++ Asteroids.Blazor.Maui/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace Asteroids.Blazor.Maui
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
